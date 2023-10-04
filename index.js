ab7a81de67231bae35d81de2ffd74d80c8fd8b3f
nada <nadayahya2020@hotmail.com>
2023-10-04 10:22:24 -0700

    change on htmle & css & work on  JS

--- index.js
+++ index.js
@@ -1,58 +1,47 @@
-const TODO = ["nada","raghad","shatha","hyugy","kiujjjj"];
+const addBtn = document.querySelector("#id-btn");
+const taskInput = document.querySelector("#back input");
+const taskCounter = document.querySelector("#tasks");
+const error = document.getElementById("MessageError");
+const countTask = document.querySelector("count-task");
 
+let taskCount = 0;
 
-
-
-
-const addList = (listInput) =>
+const displayCount = (taskCount) =>
 {
-  TODO.push(listInput);  
-  return TODO; 
+    countTask.innerText =  taskCount;
 }
 
 
 
 
-
-
-
-const deleteList = (deleteElement) =>
+const addNewTask = () =>
 {
-    for (let i = 0; i < TODO.length; i++) {
-        if (TODO[i] === deleteElement) {
-            let spliced = TODO.splice(i, 1);
-            console.log(`Remaining elements[ ${TODO} ]`);
-        }
+    const taskName = taskInput.value.trim();
+    error.style.display = "none";
+
+
+    if(!taskName)
+    {
+       setTimeout(() => {
+        error.style.display = "block";
+       }, 200);
+       return;
     }
+ const task = `
+    <div class="task">
+    <input type="checkbox" class="taskCheck">
+    <span class="taskName">${taskName}</span>
+    <button class="edit"></button>
+    <button class="delete"></button>
+    </div>
+ `
+
+ taskCounter.insertAdjacentHTML("beforeend",task)
+
 }
 
 
-
-
-
-const  createNewCheckboxt = (name, value) =>
-{
-    let checkbox = document.createElement("INPUT");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("value", value);
-    checkbox.setAttribute("name", name);
-
-    document.body.appendChild(checkbox);
-    return checkbox;
-}
-
-
-
-
-
-document.getElementById("submit-btn").addEventListener("submit", function () {
-
-    const listInput = document.getElementById("in-list").value;
-    // const newList = addList(listInput);
-    TODO.push(listInput); 
-    document.getElementById("div1").innerHTML =`${TODO}`;
-
-});
+addBtn.addEventListener("click", addNewTask() );
 
 
 
@@ -65,11 +54,6 @@ document.getElementById("submit-btn").addEventListener("submit", function () {
 
 
 
-// //TO ADD TAG ELEMENT (INPUT:CHECKBOX) TO HTML USING JS
-// const check = document.createElement("INPUT");
-// check.setAttribute("type", "checkbox");
-// const element = document.getElementById("div1")
-// element.appendChild(check);
 
 
 
@@ -77,28 +61,125 @@ document.getElementById("submit-btn").addEventListener("submit", function () {
 
 
 
-// document.getElementById("submit-btn").addEventListener("click", function () {
-//   let text = document.getElementById("in-list").value;
-//   console.log(text);
 
-//   if (text.length <= 10) {
-//     let cutFirst = text.substr(0, 1);
-//     let upper = cutFirst.toUpperCase();
-//     let cutAll = text.slice(-9);
-//     let lower = cutAll.toLowerCase();
-//     let connect = upper.concat("", lower);
 
-//     let replace = connect.replace(/a/g, "o");
-//     let revers = replace.split("").reverse().join("");
 
-//     let result = (document.getElementById(
-//       "result"
-//     ).innerHTML = `${padStar}\n${replace}\n${revers}`);
-//     console.log(result);
-//   } else {
-//     let cut = text.substring(0, 10);
-//     document.getElementById(
-//       "result"
-//     ).innerHTML = `should not be name long out of 10\n [${cut}]`;
-//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const TODO = ["nada","raghad","shatha","hyugy","kiujjjj"];
+
+
+
+
+
+// const addList = (listInput) =>
+// {
+//   TODO.push(listInput);  
+//   return TODO; 
+// }
+
+
+
+
+
+
+
+// const deleteList = (deleteElement) =>
+// {
+//     for (let i = 0; i < TODO.length; i++) {
+//         if (TODO[i] === deleteElement) {
+//             let spliced = TODO.splice(i, 1);
+//             console.log(`Remaining elements[ ${TODO} ]`);
+//         }
+//     }
+// }
+
+
+
+
+
+// const  createNewCheckboxt = (name, value) =>
+// {
+//     let checkbox = document.createElement("INPUT");
+//     checkbox.setAttribute("type", "checkbox");
+//     checkbox.setAttribute("value", value);
+//     checkbox.setAttribute("name", name);
+
+//     document.body.appendChild(checkbox);
+//     return checkbox;
+// }
+
+
+
+
+
+// document.getElementById("submit-btn").addEventListener("submit", function () {
+
+//     const listInput = document.getElementById("in-list").value;
+//     // const newList = addList(listInput);
+//     TODO.push(listInput); 
+//     document.getElementById("div1").innerHTML =`${TODO}`;
+
 // });
+
+
+
+
+
+
+
+
+
+
+
+
+// // //TO ADD TAG ELEMENT (INPUT:CHECKBOX) TO HTML USING JS
+// // const check = document.createElement("INPUT");
+// // check.setAttribute("type", "checkbox");
+// // const element = document.getElementById("div1")
+// // element.appendChild(check);
+
+
+
+
+
+
+
+// // document.getElementById("submit-btn").addEventListener("click", function () {
+// //   let text = document.getElementById("in-list").value;
+// //   console.log(text);
+
+// //   if (text.length <= 10) {
+// //     let cutFirst = text.substr(0, 1);
+// //     let upper = cutFirst.toUpperCase();
+// //     let cutAll = text.slice(-9);
+// //     let lower = cutAll.toLowerCase();
+// //     let connect = upper.concat("", lower);
+
+// //     let replace = connect.replace(/a/g, "o");
+// //     let revers = replace.split("").reverse().join("");
+
+// //     let result = (document.getElementById(
+// //       "result"
+// //     ).innerHTML = `${padStar}\n${replace}\n${revers}`);
+// //     console.log(result);
+// //   } else {
+// //     let cut = text.substring(0, 10);
+// //     document.getElementById(
+// //       "result"
+// //     ).innerHTML = `should not be name long out of 10\n [${cut}]`;
+// //   }
+// // });
